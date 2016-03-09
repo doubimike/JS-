0bbd1ec7aa0178878d4caafa603906c0a03f8242
doubimike <doubimike@qq.conm>
2016-03-09 14:36:55 +0800

    根据教程，做了优化。
    删除节点抽象成函数。

--- 人人网评论功能/js/demo.js
+++ 人人网评论功能/js/demo.js
@@ -1,23 +1,28 @@
 window.onload = function() {
-	var list = document.getElementById("list");
-	var lis = list.children;
+    var list = document.getElementById("list");
+    var lis = list.children;
 
-	// 事件代理
-	for (var i = lis.length - 1; i >= 0; i--) {
-		lis[i].onclick = function (e) {
-			 var event = e || window.event;
-			 var target = event.target || event. srcElement;
+    // 删除节点
+    function removeNode(node) {
+        node.parentNode.removeChild(node);
+    }
 
-			 var cls = target.className;
+    // 事件代理
+    for (var i = lis.length - 1; i >= 0; i--) {
+        lis[i].onclick = function(e) {
+            var event = e || window.event;
+            var target = event.target || event.srcElement;
 
-			 switch (cls) {
-			 	case "close":
-			 		this.parentNode.removeChild(this);
-			 		break;
-			 	default:
-			 		// statements_def
-			 		break;
-			 }
-		}
-	}
+            var cls = target.className;
+
+            switch (cls) {
+                case "close":
+                    removeNode(this);
+                    break;
+                default:
+                    // statements_def
+                    break;
+            }
+        }
+    }
 }
