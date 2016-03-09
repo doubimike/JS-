41283eeceee7d0844f7ff10f2255cb1e8b9110e0
doubimike <doubimike@qq.conm>
2016-03-09 13:53:42 +0800

    赤裸裸的悲剧，我写的购物车全部没了。
    不可能再自己敲一遍了。
    就这样吧。这是源代码复制过来的。
    见谅。

--- 购物车/js/js.js
+++ 购物车/js/js.js
@@ -1,17 +1,11 @@
-var g = function (id) {
-    return document.getElementById(id);
-}
-
 window.onload = function () {
-    if(!document.getElementsByClassName){
-        document.getElementsByClassName = function(cls){
+    if (!document.getElementsByClassName) {
+        document.getElementsByClassName = function (cls) {
             var ret = [];
-            var els = document.getElementsByTagName("*");
-            for (var i = 0; i < els.length; i++) {
-                if(els[i].className === cls 
-                    ||els[i].className.indexOf( cls +"") >= 0 
-                    ||els[i].className.indexOf(""+cls +"") >= 0 
-                    ||els[i].className.indexOf(""+cls) >= 0 ){
+            var els = document.getElementsByTagName('*');
+            for (var i = 0, len = els.length; i < len; i++) {
+
+                if (els[i].className.indexOf(cls + ' ') >=0 || els[i].className.indexOf(' ' + cls + ' ') >=0 || els[i].className.indexOf(' ' + cls) >=0) {
                     ret.push(els[i]);
                 }
             }
@@ -19,157 +13,154 @@ window.onload = function () {
         }
     }
 
-    var cartTable =  g("cartTable");
-    var tr = cartTable.children[1].rows;
-    var checkInputs = document.getElementsByClassName("check");
-    var checkAllInputs = document.getElementsByClassName("check-all");
-    var selectedTotal = g("selectedTotal");
-    var priceTotal = g("priceTotal");
-    var selected = g("selected");
-    var foot = g("foot");
-    var selectedViewList = g("selectedViewList");
-    var adds = document.getElementsByClassName("add");
-    var counts = document.getElementsByClassName("count");
+    var table = document.getElementById('cartTable'); // 购物车表格
+    var selectInputs = document.getElementsByClassName('check'); // 所有勾选框
+    var checkAllInputs = document.getElementsByClassName('check-all') // 全选框
+    var tr = table.children[1].rows; //行
+    var selectedTotal = document.getElementById('selectedTotal'); //已选商品数目容器
+    var priceTotal = document.getElementById('priceTotal'); //总计
+    var deleteAll = document.getElementById('deleteAll'); // 删除全部按钮
+    var selectedViewList = document.getElementById('selectedViewList'); //浮层已选商品列表容器
+    var selected = document.getElementById('selected'); //已选商品
+    var foot = document.getElementById('foot');
 
-    //计算函数
-    function getTotal () {
-    	 var selected = 0;
-    	 var price = 0;
-         var html = "";
-
-    	 for (var i = 0; i < tr.length; i++) {
-    	  	if(tr[i].getElementsByTagName("input")[0].checked){
-                selected += parseInt(tr[i].getElementsByTagName("input")[1].value);
-                price += parseFloat(tr[i].cells[4].innerHTML); 
-                //加上选中样式
-                tr[i].className = "on";
-                //预览部分加上缩略图 //tr[i].getElementsByTagName("img")[0].src 替代i+1
-                html += '<div><img src="images/'+(i+1)+'.jpg"><span class="del" index="' +i + '">取消选择</span></div>';
-
+    // 更新总数和总价格，已选浮层
+    function getTotal() {
+        var selected = 0, price = 0, html = '';
+        for (var i = 0; i < tr.length; i++) {
+            if (tr[i].getElementsByTagName('input')[0].checked) {
+                tr[i].className = 'on';
+                selected += parseInt(tr[i].getElementsByTagName('input')[1].value); //计算已选商品数目
+                price += parseFloat(tr[i].getElementsByTagName('td')[4].innerHTML); //计算总计价格
+                html += '<div><img src="'+tr[i].getElementsByTagName('img')[0].src+'"><span class="del" index="'+i+'">取消选择</span></div>';// 添加图片到弹出层已选商品列表容器
+            }else{
+                tr[i].className = '';
             }
-            else {
-                tr[i].className = "";   
-            }
-    	  }
-         selectedTotal.innerHTML = selected;
-         priceTotal.innerHTML = price.toFixed(2);  
-         selectedViewList.innerHTML = html;
-
-         //设置缩略图的取消选择 还有事件代理及冒泡的方式，写于下方
-         // var dels = document.getElementsByClassName("del");
-         // console.log(dels)
-
-         // for (var i = 0; i < dels.length; i++) {
-         //     dels[i].onclick = function () {
-         //          var index = this.getAttribute("index")  ;
-         //          tr[index].getElementsByTagName("input")[0].checked = 0;
-         //          getTotal ();
-         //     }
-         // }
-
-         // if (selectedTotal.innerHTML ==0) {
-         //     foot.className = "foot"; 
-         // }
+        }
+        selectedTotal.innerHTML = selected; // 已选数目
+        priceTotal.innerHTML = price.toFixed(2); // 总价
+        selectedViewList.innerHTML = html;
+        if (selected==0) {
+            foot.className = 'foot';
+        }
     }
 
-    //给所有选择框加上事件
-    for (var i = 0; i < checkInputs.length; i++) {
-    	checkInputs[i].onclick = function (){
-            //点击全选框的时候，所有单选框都选上，所有
-            if (this.className == "check-all check") {
-                for (var j = 0; j < checkInputs.length; j++) {
-                    checkInputs[j].checked = this.checked;
-                }
-            }
-            //一个单选框没选中，全选框也相应不选中
-            else if (!this.checked){
-                for (var k = 0; k < checkAllInputs.length; k++) {
-                    checkAllInputs[k].checked = false;
-                }
-            }
-            getTotal();
-    	}
+    // 计算单行价格
+    function getSubtotal(tr) {
+        var cells = tr.cells;
+        var price = cells[2]; //单价
+        var subtotal = cells[4]; //小计td
+        var countInput = tr.getElementsByTagName('input')[1]; //数目input
+        var span = tr.getElementsByTagName('span')[1]; //-号
+        //写入HTML
+        subtotal.innerHTML = (parseInt(countInput.value) * parseFloat(price.innerHTML)).toFixed(2);
+        //如果数目只有一个，把-号去掉
+        if (countInput.value == 1) {
+            span.innerHTML = '';
+        }else{
+            span.innerHTML = '-';
+        }
     }
 
+    // 点击选择框
+    for(var i = 0; i < selectInputs.length; i++ ){
+        selectInputs[i].onclick = function () {
+            if (this.className.indexOf('check-all') >= 0) { //如果是全选，则吧所有的选择框选中
+                for (var j = 0; j < selectInputs.length; j++) {
+                    selectInputs[j].checked = this.checked;
+                }
+            }
+            if (!this.checked) { //只要有一个未勾选，则取消全选框的选中状态
+                for (var i = 0; i < checkAllInputs.length; i++) {
+                    checkAllInputs[i].checked = false;
+                }
+            }
+            getTotal();//选完更新总计
+        }
+    }
+
+    // 显示已选商品弹层
     selected.onclick = function () {
-        if(foot.className == "foot" ){
-            if ( selectedTotal.innerHTML !=0)
-                {foot.className = "foot show"} 
-        }
-        else{
-            foot.className = "foot"; 
+        if (selectedTotal.innerHTML != 0) {
+            foot.className = (foot.className == 'foot' ? 'foot show' : 'foot');
         }
     }
 
-    //设置缩略图的取消选择-事件代理及冒泡的方式
-    selectedViewList.onclick = function (e){
-        var event = e || window.event;
-        var target = event.target || event.srcElement ;
-
-        if (target.className=="del") {
-            var index = target.getAttribute("index");
-            var input = tr[index].getElementsByTagName("input")[0];
-            input.checked = 0;
+    //已选商品弹层中的取消选择按钮
+    selectedViewList.onclick = function (e) {
+        var e = e || window.event;
+        var el = e.srcElement;
+        if (el.className=='del') {
+            var input =  tr[el.getAttribute('index')].getElementsByTagName('input')[0]
+            input.checked = false;
             input.onclick();
         }
     }
 
+    //为每行元素添加事件
     for (var i = 0; i < tr.length; i++) {
+        //将点击事件绑定到tr元素
         tr[i].onclick = function (e) {
-            var event = e || window.event;
-            var target = event.target || event.srcElement ; 
-
-            var count_input = this.getElementsByClassName("count-input")[0];
-            var reduce = this.getElementsByClassName("reduce")[0];
-
-            function ammount (obj,addorreduce) {
-
-                 count_input.value = parseInt(count_input.value) + addorreduce;
-                 count_input.innerHTML = count_input.value;
-                 calcprice (obj);
-                 if(obj.getElementsByTagName("input")[0].checked){
-                     getTotal();
-                 }
+            var e = e || window.event;
+            var el = e.target || e.srcElement; //通过事件对象的target属性获取触发元素
+            var cls = el.className; //触发元素的class
+            var countInout = this.getElementsByTagName('input')[1]; // 数目input
+            var value = parseInt(countInout.value); //数目
+            //通过判断触发元素的class确定用户点击了哪个元素
+            switch (cls) {
+                case 'add': //点击了加号
+                    countInout.value = value + 1;
+                    getSubtotal(this);
+                    break;
+                case 'reduce': //点击了减号
+                    if (value > 1) {
+                        countInout.value = value - 1;
+                        getSubtotal(this);
+                    }
+                    break;
+                case 'delete': //点击了删除
+                    var conf = confirm('确定删除此商品吗？');
+                    if (conf) {
+                        this.parentNode.removeChild(this);
+                    }
+                    break;
             }
-
-            if (target.className=="add") {
-                ammount(this,+1);
+            getTotal();
+        }
+        // 给数目输入框绑定keyup事件
+        tr[i].getElementsByTagName('input')[1].onkeyup = function () {
+            var val = parseInt(this.value);
+            if (isNaN(val) || val <= 0) {
+                val = 1;
             }
-
-            if (target.className=="reduce") {
-                if (parseInt(count_input.value) > 1) {
-                ammount(this,-1);}
+            if (this.value != val) {
+                this.value = val;
             }
-
-            if (count_input.value > 1) {
-                reduce.innerHTML = "-";
-            }
-            else {
-                reduce.innerHTML = "";
-            }
-                        
-            function calcprice (obj) {
-                var subtotal = obj.getElementsByClassName("subtotal")[0];
-                var singeprice = parseFloat(obj.getElementsByClassName("price")[0].innerHTML);
-                 subtotal.innerHTML = singeprice * parseInt(count_input.value);  
-            }
-
-            
-
-
-
+            getSubtotal(this.parentNode.parentNode); //更新小计
+            getTotal(); //更新总数
         }
     }
 
+    // 点击全部删除
+    deleteAll.onclick = function () {
+        if (selectedTotal.innerHTML != 0) {
+            var con = confirm('确定删除所选商品吗？'); //弹出确认框
+            if (con) {
+                for (var i = 0; i < tr.length; i++) {
+                    // 如果被选中，就删除相应的行
+                    if (tr[i].getElementsByTagName('input')[0].checked) {
+                        tr[i].parentNode.removeChild(tr[i]); // 删除相应节点
+                        i--; //回退下标位置
+                    }
+                }
+            }
+        } else {
+            alert('请选择商品！');
+        }
+        getTotal(); //更新总数
+    }
 
-
-
-
-
-
-
-
-
-
-
+    // 默认全选
+    checkAllInputs[0].checked = true;
+    checkAllInputs[0].onclick();
 }
