76b7b500639b5b3a755ee86842a73a00375d9e5a
doubimike <doubimike@qq.conm>
2016-03-09 15:11:17 +0800

    自己实现了点赞功能。

--- 人人网评论功能/js/demo.js
+++ 人人网评论功能/js/demo.js
@@ -12,12 +12,27 @@ window.onload = function() {
         lis[i].onclick = function(e) {
             var event = e || window.event;
             var target = event.target || event.srcElement;
+            var praisesTotal = this.getElementsByClassName("praises-total")[0];
+            var praisesTotalValue = parseInt(praisesTotal.getAttribute("total"));
 
             var cls = target.className;
 
             switch (cls) {
+                // 删除分享功能
                 case "close":
                     removeNode(this);
+                    break;
+
+                    // 点赞功能
+                case "praise":
+                    if (praisesTotalValue == 0) {
+                        praisesTotal.style.display = "block";
+                        praisesTotal.setAttribute("total", 1);
+                        praisesTotal.innerHTML = "我觉得很赞";
+                    } else {
+                        praisesTotal.setAttribute("total", praisesTotalValue+1);
+                        praisesTotal.innerHTML = "我和4个人觉得很赞";
+                    }
                     break;
                 default:
                     // statements_def
