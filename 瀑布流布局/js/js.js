f6828c2177889de205ecc8d2cf876909c7f60900
doubimike <doubimike@qq.conm>
2016-03-13 14:41:03 +0800

    实现了基本效果，可是实在找不出动态添加不同图片的bug在哪里了。
    此时有点烦躁。该调整调整心态。

--- 瀑布流布局/js/js.js
+++ 瀑布流布局/js/js.js
@@ -10,6 +10,18 @@ var getMin = function(array) {
     return [min, minindex];
 }
 
+var getMax = function(array) {
+    var max = array[0];
+
+    for (var i = 1; i < array.length; i++) {
+        if (array[i] - max > 0) {
+            max = array[i];
+
+        }
+    }
+    return max;
+}
+
 window.onload = function() {
     var windowWidth = document.body.clientWidth;
     var picWidth = 165;
@@ -17,19 +29,17 @@ window.onload = function() {
     var boxoffsetwidth = boxes[0].offsetWidth;
     var columnCount = Math.floor(windowWidth / boxoffsetwidth);
     // 获取第一行每列的高度
-    
-    var mainWidth = columnCount*boxoffsetwidth;
-    var main = document.getElementById('main');
-    main.style.cssText = "width:"+mainWidth+"px;margin:0 auto";
 
-    
+    var mainWidth = columnCount * boxoffsetwidth;
+    var main = document.getElementById('main');
+    main.style.cssText = "width:" + mainWidth + "px;margin:0 auto";
+
+
     // 封装函数 
     function setPic(array, box) {
         var min = getMin(array);
         var minheight = min[0];
         var minindex = min[1];
-        console.log(minheight)
-        console.log(minindex)
         var setleftvalue;
 
         // 设置下一张图片的位置
@@ -45,14 +55,40 @@ window.onload = function() {
 
     var columnArray = [];
     for (var i = 0; i < boxes.length; i++) {
-        if (i <= columnCount-1) {
+        if (i <= columnCount - 1) {
             columnArray.push(boxes[i].offsetHeight);
         }
 
     }
 
-    for (var i = columnCount+1; i < boxes.length; i++) {
-    	setPic(columnArray, boxes[i]);
+    for (var i = columnCount + 1; i < boxes.length; i++) {
+        setPic(columnArray, boxes[i]);
     }
-    
+
+    // 滚动加载代码
+    window.addEventListener('scroll', function() {
+
+        var scrollY = window.scrollY;
+        var mainHeight = getMax(columnArray);
+
+        var viewportHeight = document.documentElement.clientHeight;
+        console.log(viewportHeight + scrollY, mainHeight);
+
+        if (viewportHeight + scrollY > mainHeight -20) {
+        	
+            for (var i = 0; i < 24; i++) {
+                var index = boxes.length;
+                var more = document.createElement('div');
+                more.className = 'box';
+                more.innerHTML = '<div class="pic"><img src="images/' + 11 + '.jpg" alt=""></div>';
+                console.log(boxes.length)
+                main.appendChild(more);
+                setPic(columnArray, more);
+            }
+
+
+        }
+        flag = 1;
+    });
+
 }
