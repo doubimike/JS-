d910c9bb702cf70f075899fe8316141cc4932eaf
doubimike <doubimike@qq.conm>
2016-03-15 18:44:32 +0800

    理解回掉函数。

--- 动画效果/move.js
+++ 动画效果/move.js
@@ -7,25 +7,25 @@
         }
     }
 
-    function startMove(obj, des, attr) {
-
-        var self = obj;
-        clearInterval(self.timer);
-        self.timer = setInterval(function() {
+    function startMove(obj, des, attr, fn) {
+        clearInterval(obj.timer);
+        obj.timer = setInterval(function() {
                 var pI;
                 if (attr === "opacity") {
-                    pI = getStyle(self, attr);
+                    pI = getStyle(obj, attr);
                     var speed = (des < pI) ? -0.1 : 0.1;
                 } else {
-                    pI = parseInt(getStyle(self, attr));
+                    pI = parseInt(getStyle(obj, attr));
                     var speed = (des - pI) / 8;
                     speed = (speed < 0) ? Math.floor(speed) : Math.ceil(speed);
                 }
                 if (des == pI) {
-                    clearInterval(self.timer);
-                    return;
+                    clearInterval(obj.timer);
+                    if (fn) {
+                        fn();
+                    }
                 } else {
-                    self.style[attr] = (attr !== "opacity") ? (pI + speed) + "px" : (parseFloat(pI) + speed);
+                    obj.style[attr] = (attr !== "opacity") ? (pI + speed) + "px" : (parseFloat(pI) + speed);
                 }
             },
             30)
