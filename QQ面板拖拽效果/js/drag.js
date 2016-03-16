29d3e012228e03d743ad206d8636cf1033a6e150
doubimike <doubimike@qq.conm>
2016-03-16 14:27:23 +0800

    bug排除

--- QQ面板拖拽效果/js/drag.js
+++ QQ面板拖拽效果/js/drag.js
@@ -1,24 +1,53 @@
 window.onload = drag;
 
 function drag() {
-    var title = document.getElementsByClassName('login_logo_webqq')[0];
-    var panel = document.getElementById('loginPanel');
+    var title = document.getElementsByClassName('login_logo_webqq')[0],
+        panel = document.getElementById('loginPanel');
     title.onmousedown = mDown;
+    title.onmouseup = mUp;
+    var close = document.getElementById('ui_boxyClose');
+    close.onclick = function() {
+        panel.style.display = "none";
+    };
+}
 
-    function mDown(e) {
-        e = e || window.event;
-        var x = e.clientX - panel.offsetLeft;
-        var y = e.clientY  - panel.offsetTop;
-        document.onmousemove = move;
+function mDown(e) {
+    e = e || window.event;
+    var panel = document.getElementById('loginPanel'),
+        x = e.clientX - panel.offsetLeft,
+        y = e.clientY  - panel.offsetTop;
 
-        function move(e) {
-            e = e || window.event;
+    document.onmousemove = function(e) {
+        move(e, x, y);
+    }
+}
 
-            panel.style.left = (e.clientX - x) + 'px';
-            panel.style.top = (e.clientY - y) + 'px';
-        }
+function mUp() {
+    document.onmousemove = null;
+}
+
+
+function move(e, x, y) {
+    e = e || window.event;
+    var panel = document.getElementById('loginPanel'),
+        l = e.clientX - x,
+        t = e.clientY - y,
+        dh = document.documentElement.clientHeight || document.body.clientHeight,
+        dw = document.documentElement.clientWidth || document.body.clientWeight,
+        maxW = dw - panel.offsetWidth - 10,
+        maxH = dh - panel.offsetHeight;
+    if (l < 0) {
+        l = 0
+    } else if (l > maxW) {
+        l = maxW;
     }
 
+    if (t < 10) {
+        t = 10
+    } else if (t > maxH) {
+        t = maxH;
+    }
 
-
+    panel.style.left = l + 'px';
+    panel.style.top = t + 'px';
 }
