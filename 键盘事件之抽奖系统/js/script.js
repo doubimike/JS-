8e24abf168eb102a00fd632c8eab1bdd99258043
doubimike <doubimike@qq.conm>
2016-03-16 19:23:40 +0800

    代码堆好了，还是有很多可以优化的地方。

--- 键盘事件之抽奖系统/js/script.js
+++ 键盘事件之抽奖系统/js/script.js
@@ -1,19 +1,38 @@
+var timer = null;
+var flag = 1;
+
 window.onload = function() {
     var play = document.getElementById('play'),
         stop = document.getElementById('stop');
-        var timer;
+
 
 
     eventUtil.addHandler(play, 'click', function() {
-        timer = setInterval(changeContent, 30)
+        clearInterval(timer);
+        timer = setInterval(function() {
+            changeContent();
+        }, 30)
+        play.style.background = "gray";
     });
 
     eventUtil.addHandler(stop, 'click', function() {
         clearInterval(timer);
+        play.style.background = "#153168";
     });
 
-    eventUtil.addHandler(document, 'keydown', function() {
-        timer = setInterval(changeContent, 30)
+    eventUtil.addHandler(document, 'keyup', function(e) {
+        var e = e || window.event;
+        console.log(e.code)
+        if (e.code == "Enter" && flag == 1) {
+            timer = setInterval(changeContent, 30)
+            play.style.background = "gray";
+            flag = 0;
+        } else {
+            clearInterval(timer);
+            play.style.background = "#153168";
+            flag = 1;
+        }
+
     });
 
 
@@ -21,8 +40,8 @@ window.onload = function() {
 }
 
 function changeContent() {
-  var dataArray = ['苹果1','苹果2','苹果3','苹果4','苹果5','苹果6','苹果7','苹果8','苹果9','苹果10']
+    var dataArray = ['苹果1', '苹果2', '苹果3', '苹果4', '苹果5', '苹果6', '苹果7', '苹果8', '苹果9', '苹果10']
     title = document.getElementById('title');
-    var random = Math.floor(Math.random()*10);
+    var random = Math.floor(Math.random() * dataArray.length);
     title.innerHTML = dataArray[random];
 }
