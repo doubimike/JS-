899fbae146037960788de971dbdcd0af2f29f2f7
doubimike <doubimike@qq.conm>
2016-03-25 23:12:55 +0800

    block住了，bug始终解决不了。看下原理先。

--- 图片剪切效果/js/script.js
+++ 图片剪切效果/js/script.js
@@ -2,34 +2,62 @@
 
 window.onload = function() {
     var dragArea = document.getElementsByClassName('drag-area')[0],
-        frontPic = document.getElementsByClassName('front-pic')[0];
-    var x, y, oldt, oldl;
+        frontPic = document.getElementsByClassName('front-pic')[0],
+        container = document.getElementById('container'),
+        cw = container.offsetWidth,
+        ch = container.offsetHeight;
 
-    function getPos(e) {
-        x = e.clientX,
-            y = e.clientY,
+    function move(e) {
+        var oldx = e.clientX,
+            oldy = e.clientY,
             oldt = dragArea.offsetTop,
-            oldl = dragArea.offsetLeft;
+            oldl = dragArea.offsetLeft,
+            dw = dragArea.offsetWidth,
+            dh = dragArea.offsetHeight,
+            // 鼠标距离dragearea的左和高
+            disx = oldx - oldl,
+            disy = oldy - oldt;
+
+        function movePos(e1) {
+            var newx = e1.clientX,
+                newy = e1.clientY,
+                moveY = newy - oldy,
+                moveX = newx - oldx,
+                newT = moveY + oldt,
+                newL = moveX + oldl,
+            maxL = cw - dw - oldl,
+            maxT = ch - dh - oldt;
+
+            if (newL < 0) {
+                newL = 0;
+            }
+            if (newL > maxL) {
+                newL = maxL;
+            }
+
+            if (newT < 0) { newT = 0 }
+            if (newT > maxT) { newT = maxT }
+            dragArea.style.top = newT + 'px';
+            dragArea.style.left = newL + 'px';
+            frontPic.style.clip = 'rect(' + newT + 'px,' + (newL + dw) + 'px,' + (newT + dh) + 'px,' + newL + 'px)';
+        }
 
         dragArea.addEventListener('mousemove', movePos);
+
+        dragArea.addEventListener('mouseup', function() {
+            dragArea.removeEventListener('mousemove', movePos);
+        });
     }
 
-    function movePos(e) {
-        var newx = e.clientX,
-            newy = e.clientY,
-            moveY = newy - y + oldt,
-            moveX = newx - x + oldl;
-        dragArea.style.top = moveY + 'px';
-        dragArea.style.left = moveX + 'px';
-        frontPic.style.clip = 'rect(' + moveY + 'px,' + (moveX + dragArea.offsetWidth) + 'px,' + (moveY + dragArea.offsetHeight) + 'px,' + moveX + 'px)';
-    }
-
-    dragArea.addEventListener('mousedown', getPos);
 
 
 
 
-    dragArea.addEventListener('mouseup', function(e) {
-        dragArea.removeEventListener('mousemove', movePos);
-    })
+    dragArea.addEventListener('mousedown', move);
+
+
+
+    // dragArea.addEventListener('mouseout', function(e) {
+    //     dragArea.removeEventListener('mousemove', movePos);
+    // });
 }
