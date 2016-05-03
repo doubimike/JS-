1517e3707143e9bbfe19a804f9bba9be82644896
mike.zhang <mike.zhang@yeamoney.cn>
2016-05-03 18:29:24 +0800

    s

--- mongodb+ejs+mongoose+express+node+todolist/todo2/Gruntfile.js
+++ mongodb+ejs+mongoose+express+node+todolist/todo2/Gruntfile.js
@@ -1,19 +1,81 @@
-'use strict';
-
 module.exports = function(grunt) {
     grunt.initConfig({
-        watch: {
-            css: {
-                files: ['public/css/*.scss'],
-                tasks: ['sass:dev']
-            },
-            js: {
-                files: ['public/js/*.js'],
-                tasks: ['uglify:dev']
-            }
+        pkg: grunt.file.readJSON('package.json'),
+
+        jshint: {
+          options: {
+            reporter: require('jshint-stylish') // use jshint-stylish to make our errors look and read good
+          },
+
+          // when this task is run, lint the Gruntfile and all js files in src
+          build: ['Gruntfile.js', 'public/**/*.js']
         },
+
+        uglify: {
+          options: {
+            banner: '/*\n <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> \n*/\n'
+          },
+          build: {
+            files: {
+              'dist/js/script.min.js': 'public/**/*.js'
+            }
+          }
+        },
+
+        sass: {
+          build: {
+            files: {
+              'public/css/style.css': 'public/css/style.scss'
+            }
+          }
+        },
+
+         cssmin: {
+          options: {
+            banner: '/*\n <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> \n*/\n'
+          },
+          build: {
+            files: {
+              'dist/css/style.min.css': 'public/css/style.css'
+            }
+          }
+        },
+
+        watch: {
+            options: {
+              livereload: true,
+            },
+          
+          stylesheets: { 
+          files: ['public/**/*.css', 'public/**/*.scss'], 
+          tasks: ['sass', 'cssmin'] },
+
+          // for scripts, run jshint and uglify 
+          scripts: { 
+            files: 'public/**/*.js', tasks: ['jshint', 'uglify'] 
+          } 
+        },
+
+        nodemon: {
+            dev: {
+                scripts: './bin/www'
+            }
+        }
+
+
 
     });
 
+grunt.loadNpmTasks('grunt-nodemon');
+    grunt.registerTask('default', ['nodemon']);
+
+// grunt.registerTask('default', ['jshint', 'uglify', 'cssmin', 'sass']); 
+
     grunt.loadNpmTasks('grunt-contrib-watch');
-}
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-compass');
+};
---
+++ mongodb+ejs+mongoose+express+node+todolist/todo2/public/js/script2.js
@@ -0,0 +1,4 @@
+// test
+var a =1;
+
+var test=2;
