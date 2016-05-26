94661978ece172d514771d3df3ba16cf2b571604
zhanghang <mike.zhang@yeamoney.cn>
2016-05-27 07:41:41 +0800

    t
    t

--- express入门/scotchExpress/server.js
+++ express入门/scotchExpress/server.js
@@ -3,7 +3,9 @@ var app = express();
 var bodyParser = require('body-parser');
 
 
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 app.use(bodyParser.json());
 
 var port = process.env.PORT || 8080;
@@ -16,28 +18,75 @@ var Bear = require('./app/models/bear');
 
 var router = express.Router();
 
-router.use(function (req,res,next) {
-	 console.log('Something is happening.') ;
-	 next();
+router.use(function(req, res, next) {
+    console.log('Something is happening.');
+    next();
 });
 
-router.get('/',function (req,res) {
-	 res.json({message:'hooray! welcome to our api!'}) ;
+router.get('/', function(req, res) {
+    res.json({
+        message: 'hooray! welcome to our api!'
+    });
 });
 
 router.route('/bears')
-	.post(function (req,res) {
-		 var bear = new Bear() ;
-		 bear.name = req.body.name;
+    .post(function(req, res) {
+        var bear = new Bear();
+        bear.name = req.body.name;
 
-		 bear.save(function (err) {
-		 	 if (err) {res.send(err)} ;
-		 	 res.json({message:'Bear created!'});
-		 })
-	});
+        bear.save(function(err) {
+            if (err) {
+                res.send(err)
+            };
+            res.json({
+                message: 'Bear created!'
+            });
+        });
 
-app.use('/api',router);
+    })
+    .get(function(req, res) {
+        Bear.find(function(err, bears) {
+            if (err) {
+                res.send(err)
+            };
+            res.json(bears);
+        });
+    });
+
+router.route('/bears/:bear_id')
+    .get(function(req, res) {
+        Bear.findById(req.params.bear_id, function(err, bear) {
+            if (err) {
+                res.send(err);
+            }
+            res.json(bear);
+        });
+    })
+    .put(function(req, res) {
+        Bear.findById(req.params.bear_id, function(err, bear) {
+            if (err) res.send(err);
+            bear.name = req.body.name;
+
+            bear.save(function(err) {
+                if (err) res.send(err);
+                res.json({
+                    message: 'Bear updated!'
+                });
+            });
+
+        });
+    }).delete(function(req, res) {
+        Bear.remove({
+            _id: req.params.bear_id
+        }, function(err, bear) {
+            if (err)
+                res.send(err);
+
+            res.json({ message: 'Successfully deleted' });
+        });
+    });
+
+app.use('/api', router);
 
 app.listen(port);
-console.log('Magic happens on port '+port);
-
+console.log('Magic happens on port ' + port);
