caa6f63d70b4354530ac523a29403c9a203a8ee1
mike.zhang <mike.zhang@yeamoney.cn>
2016-05-27 16:52:43 +0800

    s
    s

---
+++ reactLearn/ruanyifeng/public/scripts/script.js
@@ -0,0 +1,22 @@
+var names = ['a', 'b', 'c']
+var arr = [<h1>Hello world!</h1>,
+	<h2>React is awesome</h2>
+];
+
+
+ReactDOM.render(
+	<div>
+		{
+			arr
+		}
+	</div>,
+	document.getElementById('example')
+);
+
+
+var a;
+var a;
+
+function a(argument) {
+	// body...  
+}
